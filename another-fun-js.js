4881913afaf74eed30d8753e34bbc5a6a7423c09
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-04 12:48:29 -0600

    working the idea

--- another-fun-js.js
+++ another-fun-js.js
@@ -1,2 +1,11 @@
 "use strict";
 
+// Make a function named isLowerCase(letter) which returns true if the parameter is lowercased; otherwise it returns false
+// Write a function named double(n) that returns a number times two
+// Make a function named areIdentical(input1, input2) that returns true if both inputs are same value and data type; otherwise it returns false.
+
+function showName(username) {
+    alert(username);
+
+    showName(getName());
+}
