17986e7318018a584f86e83f99dc0dd9d1e44644
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-04 11:36:10 -0600

    refactoring file tree because i didnt drink coffee

---
+++ js/my-first-js-file.js
@@ -0,0 +1,14 @@
+"use strict";
+//
+// //console.log("This is fine")
+//
+let myName = "Mike";
+
+myName = " Stokes";
+
+let myName = prompt("Hi, what's your name?");
+
+console.log(myName.toLocaleUpperCase());
+
+
+alert("Hope you didn't console log this")
