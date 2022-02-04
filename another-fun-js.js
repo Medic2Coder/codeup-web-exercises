803ff5fe27366560df93a796c99cf52958499510
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-04 12:54:31 -0600

    making this way too complicated

--- another-fun-js.js
+++ another-fun-js.js
@@ -4,8 +4,14 @@
 // Write a function named double(n) that returns a number times two
 // Make a function named areIdentical(input1, input2) that returns true if both inputs are same value and data type; otherwise it returns false.
 
-function showName(username) {
-    alert(username);
-
-    showName(getName());
+function (isLowerCase) {
+    callFunction();
+    return true;
 }
+//There probably needs to be a datasets or arrays of lowercase and uppercase letters to assign to this functions
+('a').click(function () {
+    callFunction();
+    return false;
+});
+
+//There probably needs to be a datasets or arrays of lowercase and uppercase letters to assign to this functions
