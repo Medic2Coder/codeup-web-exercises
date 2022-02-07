e26cacf4a014c4893b977b529fe7837c30bbf6aa
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-07 10:59:12 -0600

    conditionals walkthrough

---
+++ conditionals-lec.js
@@ -0,0 +1,36 @@
+"use strict";
+
+//class notes: name ==="xavier"
+//based on this condition you can either have a or b branches
+//T exectues some code, False executes some code
+//the most complex logic is based on very small decisions
+//in essence executing specific blocks of code
+
+let isAdmin = false;
+
+if (isadmin) {
+    console.log("You're an admin")
+}
+
+let itemPrice = 4.99;
+let moneyInMyWallet = 5.00;
+
+if (moneyInMyWallet > itemPrice) {
+    console.log("You can buy this itemPrice")
+} else {
+    console.log("You can't buy this ya poor")
+}
+
+//can I buy a drink at the bar?
+//gather info, details that will become variables
+//drinking age, price of beer
+var age = 21;
+var priceOfDrink = 8.00;
+
+if age >= 21 && moneyInMyWallet >= priceOfDrink) {
+    console.log("You can buy a drink")
+} else {
+    console.log("Nope")
+}
+
+//note to self: console log issues? can't see this running in console, so is this syntax or operator error, intellij error/
