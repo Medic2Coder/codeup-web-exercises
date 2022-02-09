4a1da49d04238c66012ba0da9691218dbe51975f
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-09 11:46:23 -0600

    helloWorld testing

--- js/tests.js
+++ js/tests.js
@@ -1,2 +1,14 @@
 // Unit tests for the helloWorld function
 "use strict";
+
+//helloWorld Function
+
+function helloWorld() {
+    return "Hello, World!";
+}
+
+function sayHello(name) {
+    if (name === "Jane") {
+        return "Hello, Jane";
+    }
+}
