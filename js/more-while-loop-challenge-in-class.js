4eb3b076b42f957854927a144f71ec0d7a4db2c5
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-09 16:07:46 -0600

    do while practice in class

---
+++ js/more-while-loop-challenge-in-class.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+
+
+
+// // TODO: A parking lot with 30 parking spaces wants to display the amount of spaces available as it gets filled.
+//  Let's assume that they are allowing two cars into the parking lot at a time. As those cars take up spaces,
+//  write a do-while loop that prints the spaces available. If the parking lot gets filled, print "this parking lot
+//  is now full" to the console.
+//
+
+let numspaces = 30;
+console.log(Welcome to the OverPricedParking Garage)
+do {
+    console.log("There are " + numspaces + " remaining!")
+    numspaces -=2;
+} while (numspaces);
+
+
+console.log(numspaces);
+
+// //TODO: using prompt() and alert(), write a while loop that will continue to ask for the correct password until the
+//  correct password is given.
+
+
+//TODO: For Self - will not run in liveserver mode
+//TODO: For Self - will not equate to Pricing Garage even when using Casey's example above ^ what is missing
--- js/while-loop-class-challenge-name-app.js
+++ js/while-loop-class-challenge-name-app.js
@@ -1,2 +1,37 @@
 "use strict";
 
+let fullNameEntered = false;
+let names = "";
+// WE DON'T KNOW HOW MANY NAMES THEY HAVE!
+//Use the right tool for the right job, because the while loop was not the best tool, where as do while was
+do {
+    names += prompt("Please enter or add to your name");
+    fullNameEntered = confirm("Is this your full name?");
+    if(!fullNameEntered){
+        names+= " ";
+    }
+}
+alert("This is your name: " + names);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
