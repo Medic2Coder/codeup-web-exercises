b0b37a788c1c52c37248116fce61c3b031393eba
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-09 14:45:20 -0600

    getting odd numbers from 8 to 50

--- js/loop-exercise-challenge-in-class.js
+++ js/loop-exercise-challenge-in-class.js
@@ -7,8 +7,14 @@
 // console.log("The loops has ended");
 
 //TODO: Use a while loop to output the numbers from 8-50
+//TODO: Using the same example, output the ODD numbers from 8-50
+//TODO: Hint: Use the % operator..
+// let i = 8;
+// while (i <=50) {
+//     console.log(i++);
+// }
 
-let i = 8;
-while (i <=50) {
-    console.log(i++);
+for (let i = 8; i<=50; i++) {
+  if ( i % 2 !== 0) console.log(i);
 }
+
