269ca280488657479255fd9e376e39acc7f70345
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-09 14:35:07 -0600

    loop challenge

---
+++ js/loop-exercise-challenge-in-class.js
@@ -0,0 +1,10 @@
+"use strict";
+//This is walkthrough portion
+let incrementor = 0;
+while (incrementor > 0) {
+    console.log(incrementor--);
+}
+console.log("The loops has ended");
+
+//TODO: Use a while loop to output the numbers from 8-50
+
