eecf5240f65d98c2f0930f788481ebfc774a3c13
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-10 16:18:38 -0600

    string repeat method talked about in class

--- js/for-loops.js
+++ js/for-loops.js
@@ -30,9 +30,27 @@
 // let randomNum syntax not correct
 
 
-function printNumStack(){//casey's Walkthrough in class // nested for loop?
-    for (let i =1; i < 10; i++){
-        console.log(i);
-    }
-}
-printNumStack();
+// function printNumStack(){//casey's Walkthrough in class // nested for loop?
+//     for (let i =1; i < 10; i++){
+//         console.log(i);
+//     }
+// }
+// printNumStack();
+
+// function printNumStack(){//casey's Walkthrough in class // nested for loop?
+//     for (let outerNum =1; outerNum < 10; outerNum++){
+//         for (let innerNum =1; innerNum <= outerNum; innerNum++)
+//             console.log(outerNum);//not formatted correctly? // Make these a string?
+//         // console.log(i);
+//     }
+// }
+// // printNumStack();
+
+//Note to self to review primer on JS with freecodecamp to plug holes in JS knowledge
+
+for (var x = 1, y = 1; x <10; x++, y++) {
+    let output = x.toString();
+    console.log(output.repeat(y));//Jordan Foote's example
+}// Casey comments that this uses multiple control variables in a loop
+// however what object is the repeat on? notice it is on string.repeat so output
+//type string then dot eg string. and you see all other examples of this use
