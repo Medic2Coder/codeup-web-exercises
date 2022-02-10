eb47d154181abf1d670faf7f72e7efaac1f24b64
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-10 11:30:35 -0600

    printing even numbers

---
+++ js/for-loop-even-numbers-and-odd.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// TODO: Use a for loop to print the even numbers between 0 and 30, inclusive.
+//  HINT: remember the remainder operator (modulo)
+
+
+let i;
+i = 0;
+while (i<=30) {
+    i+=2;
+    console.log(i);
+}
+
+
+// TODO: loop through numbers 1 - 40. Print "marco" if it's divisible by 3. Print "polo" if it's divisible by 5.
+//  -> If it's neither print "I'm not playing"
+//  -> Print "marco polo" if it's divisible by 3 and 5.
+
