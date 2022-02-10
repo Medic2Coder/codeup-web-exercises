c90813f0b80687e7a3b320d80ee3fdeb80621844
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-10 16:06:09 -0600

    update

--- js/for-loops.js
+++ js/for-loops.js
@@ -12,19 +12,27 @@
 
 // TODO: What is the syntax error preventing this to console.log? ^
 
-function printRandomNumbers (num) {
-    for (let i = 1; i <=10; i++){
-        // let randomNum = (Math.floor()*180) =20
-        // let randomNum =math.floor(180 * Math.random()=20)
-        // let randomNum = Math.floor()*180=20
-        randomNum
-        if (randomNum % 2 === 0) {
-            console.log(randomNum + " is even!");
-        }else{
-            console.log(RandomNum + " is odd!");
-        }
-    }
-}
+// function printRandomNumbers (num) {
+//     for (let i = 1; i <=10; i++){
+//         // let randomNum = (Math.floor()*180) =20
+//         // let randomNum =math.floor(180 * Math.random()=20)
+//         // let randomNum = Math.floor()*180=20
+//         randomNum
+//         if (randomNum % 2 === 0) {
+//             console.log(randomNum + " is even!");
+//         }else{
+//             console.log(RandomNum + " is odd!");
+//         }
+//     }
+// }
 // Casey's example
 //more syntax errors
 // let randomNum syntax not correct
+
+
+function printNumStack(){//casey's Walkthrough in class // nested for loop?
+    for (let i =1; i < 10; i++){
+        console.log(i);
+    }
+}
+printNumStack();
