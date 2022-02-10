e5628107e90de2c384c28a84ffb9e6dca69fe7fb
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-10 12:33:35 -0600

    marco polo

--- js/for-loop-even-numbers-and-odd.js
+++ js/for-loop-even-numbers-and-odd.js
@@ -2,13 +2,13 @@
 
 // TODO: Use a for loop to print the even numbers between 0 and 30, inclusive.
 //  HINT: remember the remainder operator (modulo)
-
-
+for (let i =0; i<=30; i+=2){
+        console.log(i);
+}
 // let i;
-// i = 0; //add a for to this
 // while (i<=30) {
-//     i+=2;
-//     console.log(i);
+// i = 0; //add a for to this
+//  console.log(i);
 // }
 
 
@@ -17,14 +17,16 @@
 //  -> Print "marco polo" if it's divisible by 3 and 5.
 
 for (let i = 0; i <=40; i++){
-    if (i%3 === 0){
+    if (i%3 ===0 && i%5 ===0)
+        console.log(i+" Marco Polo!")
+    else if (i%3 === 0){
         console.log(i+"marco");
     } else if (i%5===0){
         console.log(i + "polo");
-    } else if (i%3 ===0 && i%5 !==0){
+    } else {
         console.log(i +"I'm not playing... this isn't fun :(");
-    }  if (i%3 ===0 && i%5 ===0)
-        console.log(i+" Marco Polo!")
+    }
 }
 
 //Strange console log results, 3569101215
+//fixed with elegant solution suggestion of placing else and console log statement
