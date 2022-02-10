4b1d1b3dc76914803253ef9d645ef596009f21d9
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-10 10:36:29 -0600

    loops-lec-class-notes

---
+++ js/loops-lec.js
@@ -0,0 +1,30 @@
+"use strict";
+//Personal notes
+//let num = 8;
+
+//
+
+//for (num; num <=50; num++){
+// console.log(num)
+// }
+
+for (/*control variable*/; /*test condition*/; /*iterator*/) {
+    //code to run for each iteration of the loop
+}
+
+// Identify one control variable
+// Test the  two run condtion
+// Iterate through
+// Repeat through
+
+// 1. Control Variable - thing we keep checking against
+// 2. condition, thing we check against control variable, like num is less than 50
+// 3. Iterates
+// 4. loops
+// 5. Break the loop
+
+
+// for 8; and 8 is less than 50, for num 8 +1
+// check that nine is ...for (
+//     whenever this condition turns false we break the loop
+// )
