8904e6544ec593479ce4b93b9aeb93860000fe77
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-10 17:12:35 -0600

    update

---
+++ js/array-fun.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//Casey's walkthrough
+
+let myNiceArr = ["Casey", "Mark", "Ry", "Larry"];
+
+for (let i = 0; i <= myNiceArr; i++){
+    console.log("Hello, " + myNiceArr[i]);
+}
+    //Notes:
+    //Always remember:
+    //index starts at 0
+    //length starts at 1
+
+    // to make it repeatable in a loop, start with i that is 0
+// the length will be four, but four does not exist because the index is three.
+
+    //undefined in javascript but in other languages will yell out you saying out of range
