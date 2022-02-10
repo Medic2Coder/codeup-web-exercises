6c5a2300a22f593d5d06421152c83cc8692f0ad2
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-10 15:02:30 -0600

    randomNum syntax

---
+++ js/for-loops.js
@@ -0,0 +1,29 @@
+"use strict";
+
+//Casey's walkthrough and notes
+
+// function showMultiplicationTables (num) {
+//     for (let multiplier=1; multiplier <=10; multiplier++) {
+//         //7 x 1 = 7
+//         console.log(num + " x " + multiplier + "=" +num *multiplier);
+//         // console.log(num*multiplier);
+//     }
+// }
+
+// TODO: What is the syntax error preventing this to console.log? ^
+
+function printRandomNumbers (num) {
+    for (let i = 1; i <=10; i++){
+        // let randomNum = (Math.floor()*180) =20
+        // let randomNum =math.floor(180 * Math.random()=20)
+        let randomNum = Math.floor()*180=20
+        if (randomNum % 2 === 0) {
+            console.log(randomNum + " is even!");
+        }else{
+            console.log(RandomNum + " is odd!");
+        }
+    }
+}
+// Casey's example
+//more syntax errors
+// let randomNum syntax not correct
