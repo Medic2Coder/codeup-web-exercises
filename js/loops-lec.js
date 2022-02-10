6867daa24a2894eb4bc9178b6f02f5498336b9be
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-10 10:45:51 -0600

    solving the 36 character problem

--- js/loops-lec.js
+++ js/loops-lec.js
@@ -8,9 +8,9 @@
 // console.log(num)
 // }
 
-for (/*control variable*/; /*test condition*/; /*iterator*/) {
-    //code to run for each iteration of the loop
-}
+// for (/*control variable*/; /*test condition*/; /*iterator*/) {
+//     //code to run for each iteration of the loop
+// }
 
 // Identify one control variable
 // Test the  two run condtion
@@ -28,3 +28,58 @@ for (/*control variable*/; /*test condition*/; /*iterator*/) {
 // check that nine is ...for (
 //     whenever this condition turns false we break the loop
 // )
+
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+
+let letters = "";
+
+for(let i = letters.length - 1; i <=36; i++){ //stop the loop when the character count is 36
+    letters += "z"
+    console.log(letters); //we are not trusting i is in parity with letters?
+}
+//i<=35 ?
+//
+// or i --; = nope, don't do this! this is infinie
+//
+// or letters.lenth -1 in for line (nope that gave us 38 in console log!
+//How to solve so we end up with 36?
+console.log(letters.length);
+
+
+
+
+
+
+
