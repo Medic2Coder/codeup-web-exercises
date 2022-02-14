d679fd9780692f8bdd08012a7ef5b1bb3b3e7a7f
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-14 10:53:57 -0600

    sorted

--- js/planets-array.js
+++ js/planets-array.js
@@ -1,10 +1,10 @@
 "use strict";
 
-(function(){
-    "use strict";
-
-    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-    var planetsArray;
+// (function(){
+//     "use strict";
+//
+//     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+//     var planetsArray;
 
     /**
      * TODO:
@@ -12,8 +12,13 @@
      * planetsArray.
      * console.log planetsArray to check your work
      */
-
+// var planetsArray = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune",] //this was done manually
+// so to save time one can use split:
+let planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+let planetsArray = planetsString.split("|");
+// console.log(planetsArray);
     // console.log(planetsArray);
+    // console.log(planetsArray)
 
     /**
      * TODO:
@@ -21,9 +26,39 @@
      * results. Why might this be useful?
      *
      * BONUS:
-     * Create another string that would display your planets in an undordered
+     * Create another string that would display your planets in an unordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+    // let listPlanets = planetsString.split("|");
+    // let breakListPlanets = "<ul>" + "<li>" + listPlanets.join("</li><li>") + "</li>" + "</ul>";
+    // console.log(listPlanets);
+    // console.log(breakListPlanets);
 
-})();
+    //Note to self, then would this work to make an ordered list with same logic?
+//let's try
+
+let listPlanets = planetsString.split("|");
+// let breakListPlanets = "<ol>" + "<li>" + listPlanets.join("</li><li>") + "</li>" + "</ol>"; //not working
+// let breakListPlanets = "<ul>" + "<li>" + listPlanets.join("</li><li>") + "</li>" + "</ul>";//trying ul again, still not working
+console.log(listPlanets);
+// console.log(breakListPlanets);// not working
+//
+// Collections.sort(list);
+
+//results but not actually ordered, how to make it alphabetical?
+// int[] arr = listPlanets;
+// ins[] arr = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus",  "Neptune"]
+// let arr = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus",  "Neptune"]
+// Arrays.sort(arr);
+// System.out.println("Modified arr[] : %s",
+//     Arrays.toString(arr));
+
+
+
+listPlanets.sort();
+console.log(listPlanets); //Much better
+
+
+
+// })();
