9556e0331ee6d1a2a1e885b7bb684ec70c82e7ad
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-14 11:54:56 -0600

    walkthrough for objects in js

---
+++ js/objects-lec-walkthrough-notes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//Creating an object
+
+let car = {}
+    console.log(typeof car);
+
+car.make = 'Ford';
+
+console.log(car);
+
+car.model = 'Mustang';
+
+console.log(car);
+
+//Assigning Properties aka keys to an Object
+//objects are assigned using the curly brackets
+
+console.log(car.model);
+
+
