d0a5d95b3b5b0c77065431ffdb1b22da0a5a3bc1
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-14 09:22:01 -0600

    warmup js

---
+++ js/warmup_14Feb2022.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Write a function called findAverage that takes in an array of integers representing grades and returns the average.
+//     Example input: [95, 74, 86, 100]
+// Example output: 88.75 (edited)
+
+function findAverage(){//create a bucket
+    let sum = 0;
+    //loop through grades, where we add each grade to the sum
+    grades.forEach(function (grade) {
+        sum +=grade;
+    }
+    for (let i = 0; i < grades.length; i++) {}
+    retrun.sum /grades.length;
+
+}
+
+console.log(findAverage) [95, 74, 86, 100]
+
+//syntax
