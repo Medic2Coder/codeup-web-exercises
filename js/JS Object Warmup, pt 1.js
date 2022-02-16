79a5d6e9bc5b0c0fa70c32c0080dcdc88b3a6952
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-16 09:22:02 -0600

    names and occupation added

--- js/JS Object Warmup, pt 1.js
+++ js/JS Object Warmup, pt 1.js
@@ -7,11 +7,14 @@
 // Hard code some values in that object, then log it and its properties to the console
 
 const Mark = {
+    firstname: "Markle",
+    lastname: "Sparkle",
     species: 'human',
     model: 'north american',
-    year: 1986,
+    year: 1999 + "wishful thinking",
     eyes: 'brown',
-    password: "fortran4life"
+    password: "fortran4lie",
+    occupation: "Padawan to code gremlin"
 
 };
 
