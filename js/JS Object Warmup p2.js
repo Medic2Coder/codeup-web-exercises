d4523bdd6d1ffb3a2b8d3e93704c0a41513d2112
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-16 09:46:20 -0600

    JS Object warmup p2 prior to walkthrough

---
+++ js/JS Object Warmup p2.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// JS Object Warmup, pt 2:
+// Create an object representing a person’s contact info. It should have:
+//     phone number
+// street address
+// state
+// postal code
+// email address
+//
+
+let contactInfo = {
+    streetAddress: ["8888 Research Forest Drive", + "Houston, Texas" + 77382]
+    state: "Texas",
+    postalCode: 77382,
+    emailAddress: "somethingsomething@gmail.com"
+
+}
+
--- js/JS Object Warmup, pt 1.js
+++ js/JS Object Warmup, pt 1.js
@@ -7,6 +7,7 @@
 // Hard code some values in that object, then log it and its properties to the console
 
 const Mark = {
+    fullname:["Markle", "M.", "Sparkle"],
     firstname: "Markle",
     lastname: "Sparkle",
     species: 'human',
@@ -14,9 +15,24 @@ const Mark = {
     year: 1999 + "wishful thinking",
     eyes: 'brown',
     password: "fortran4lie",
-    occupation: "Padawan to code gremlin"
-
+    occupation: "Padawan to code gremlin",
+    dob: "10.21.1999"
 };
 
 console.log(Mark);
 console.log(Mark.password);
+//
+
+//walkthrough by Casey:
+
+// let person2 = Object.create(person); //syntax error........
+//
+// person2["names"]=["ry", "sutton"];
+// person2["occupation"]="Fab Father";
+// person2.dob ="01.01.1998"
+// console.log(person2);
+
+
+//
+
+
