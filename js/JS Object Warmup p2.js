cb2b23fc43c5c2e3cefe94a040ecc2cb09002087
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-16 10:13:22 -0600

    update to warmup pt 2

--- js/JS Object Warmup p2.js
+++ js/JS Object Warmup p2.js
@@ -10,11 +10,28 @@
 //
 
 let contactInfo = {
-    streetAddress: ["8888 Research Forest Drive", + "Houston, Texas" + 77382], //why a NaN here?
+    // streetAddress: ["8888 Research Forest Drive", + "Houston, Texas" + 77382], // //why a NaN here?
+    streetAddress: "8888 Hazel Lane",
     state: "Texas",
+    city: "Houston",
     postalCode: 77382,
     emailAddress: "somethingsomething@gmail.com"
 
 }
 
 console.log(contactInfo);
+console.log(contactInfo.city + "," + contactInfo.state + ","); //there is an easier way
+console.log(typeof this);
+
+//TODO: print out the formatted address of the contact info
+// It should look like this-ish:
+
+//123 anywhere Ln
+
+//constructor function:
+
+// function Person(names, occupation, dob) {
+//     this.streetAddress = streetAddress;
+//     this.postalCode = postalCode;
+//     this.dob = dob;
+// }
