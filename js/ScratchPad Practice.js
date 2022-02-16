1cef4218001000493c1295b8306e8072d2517ec2
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-16 06:43:09 -0600

    notes update

--- js/ScratchPad Practice.js
+++ js/ScratchPad Practice.js
@@ -12,3 +12,40 @@ and answer that, Question and answer style if necessary to be able to speak it o
  */
 //Started: 16Feb2022 0530
 
+// function testFun(param1, param2){
+//     console.log(1, 2);
+// }
+//
+// testFun(1,2);
+
+//Much of this work is inspired by the work I did in FreeCodeCamp
+function reusableFunction(){
+    console.log("Hi World");
+}
+reusableFunction();
+
+function functionWithArgs(parameter1,parameter2){
+    console.log(parameter1 + parameter2);
+}
+functionWithArgs(1,2);
+functionWithArgs(7,9);
+
+
+
+
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    return arr.shift(item);
+    // Only change code above this line
+}
+
+// Setup
+const testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr)); //To make a string from an array use JSON.stringify
+
+
