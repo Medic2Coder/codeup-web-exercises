b24731a31f3e8df8e4769fb0c41e12161fd9dcaf
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-16 05:43:53 -0600

    Practice sheet added

---
+++ js/ScratchPad Practice.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//This is my scratchpad for practicing JS, for refreshing knowledge and building on that knowledge.
+
+/*This will most likely not be extremely organized as this is for free flow externalizing of JS thoughts and to improve
+muscle memory in typing out this beautiful functions.
+ */
+
+/* Attempting to be as "Feynman Technique" as possible, ie, starting with a blank note and writing everything about the topic
+plainly and simply. To advance this, place questions that myself or a child would have when introduced to this knowledge
+and answer that, Question and answer style if necessary to be able to speak it out or use Socratic method.
+ */
+//Started: 16Feb2022 0530
+
