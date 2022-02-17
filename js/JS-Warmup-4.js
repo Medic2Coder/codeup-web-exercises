b99f60aa60bbe76bdc196e825e34edb55f776455
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-17 09:20:57 -0600

    psuedocoding myself

--- js/JS-Warmup-4.js
+++ js/JS-Warmup-4.js
@@ -1,13 +1,14 @@
 "use strict";
 // JS Object Warmup, pt 4:
 // We are going to create a method which
-
+let name = window.prompt("Enter all your names to your heart's content: ");
+alert("Your name is " + name);
 // allows a user to input their desired amount of names names
 
 // use prompt, alert, confirm inside of a while loop
 
 //     assign each name to an array
-
+let name = [];
 // once the user decides they are done giving names, end the loop and return the string array
 
 // then assigning that returned array by invoking your new function on the Person.names property value!
