dc12a23c6bbf7fe1b74fc86e1948c0ed9b599fbb
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-18 09:37:10 -0600

    NaN function

--- js/true or false function fun with numbers.js
+++ js/true or false function fun with numbers.js
@@ -2,8 +2,16 @@
 
 
 // Create a function which returns true if the given argument is a number, false if it is not a number
-//
+//Testcases to consider:
+// Numeric strings?
+// Boolean?
+// Arrays?
+// Objects?
+// To test, simply invoke your function inside a console.log and pass in values of different types (see above) as arguments
+
+
 // function trueForNumber(x){
+//     x === 3;
 //     if (x) === !NaN);
 //     return true;
 //     else
@@ -22,9 +30,39 @@
 //     return isNaN(c||typeof(c)!=="number"?false:true; //syntax?
 // }
 
-var numerical = function(y) {
-    if(isNaN(y)===false) {
-        return true;
-    } else
-        return false; //favorite
-};
+// function numerical (y) {
+//     y=2;
+//     if(isNaN(y)===false) {
+//         return true;
+//     } else
+//         return false; //favorite
+// }
+//
+//
+// console.log(numerical);
+
+// Casey's method'
+
+function isANumber(maybeANum){
+    // console.log(Number.isNaN(maybeANum));
+    // return !isNaN(maybeANum); // give opposite of the opposite of truthiness
+    return !!parseFloat(maybeANum);
+}
+
+//string
+console.log(isANumber("zebra"));
+
+// numeric string
+console.log(isANumber("12"));
+
+
+
+function definitelyNumerical(numberOrNot){ //making the function my own for memory purposes
+    return !!parseFloat(numberOrNot);
+
+}
+//numeric string
+console.log(definitelyNumerical("132"));
+//mary poppins typo
+console.log(definitelyNumerical("super cal i fridgelicious"));
+
