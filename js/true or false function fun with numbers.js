d5311499ee78f024a353568667f355cde902462a
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-18 09:19:27 -0600

    need java beans for js

---
+++ js/true or false function fun with numbers.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+// Create a function which returns true if the given argument is a number, false if it is not a number
+//
+// function trueForNumber(x){
+//     if (x) === !NaN);
+//     return true;
+//     else
+//     prompt("false!") ; //first thought process
+// }
+//
+// Function Numeric(a) {
+//     if(isNaN(a)===false)
+//         return true;
+//     else
+//         throw "Element is not a number"; //stackflow inspired
+// }
+//
+// function isNumber(c)
+// {
+//     return isNaN(c||typeof(c)!=="number"?false:true; //syntax?
+// }
+
+var numerical = function(y) {
+    if(isNaN(y)===false) {
+        return true;
+    } else
+        return false; //favorite
+};
