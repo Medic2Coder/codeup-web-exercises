d808d1a20a437b55e78fc6afad25f515d22866b7
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-28 09:33:42 -0600

    warmup

---
+++ 3_js/JavaScript-Warmup-Feb-28.js
@@ -0,0 +1,40 @@
+
+// var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+//
+// console.log(removeAll(bugs, "ant"));
+//
+// // should print
+// [
+//     'mosquito',
+//     'scorpion',
+//     'mosquito',
+//     'typo',
+//     'reference error',
+//     'type error'
+// ]
+
+// Warmup: Write a function called removeAll that takes two parameters.
+//     One that represents an array and another that represents a value to be removed from that array.
+//     When called this function should return an array with all the values of the original array except
+// the specified value to be removed.
+
+
+              //Walthrough below... however still want to use pop
+
+function removeAll (coffees, valueToRemove) {
+    let coffees = ['espresso', 'mochachino', 'cocoa', 'jetblackhyperchargedcoffee'];
+
+    for (let i = 0; i <coffees.length; i++) {
+    if (valueToRemove === coffees[i]) }
+    continue;
+}
+filtered.push(coffees[i]);
+
+console.log(removeAll(coffees, "cocoa"));
+
+// TODO: Refactor this
+
+
+
+
+
