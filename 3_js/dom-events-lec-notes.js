7c4b1365ec425f267d0711c9a0d41aefd29a0d70
Michael Stokes <michael.stokes.347@gmail.com>
2022-03-07 11:52:24 -0600

    notes on dom events

---
+++ 3_js/dom-events-lec-notes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let handler = function () {
+    alert("You clicked something!");
+
+}
+
+document.addEventListener("click", handler);
+
+//function expressions are NOT hoisted to the top of the script -
+// ...therefore best practice to put for example document handler on bottom
+
+//This is a better way to limit scope, best practice ^
+
+//remember that regular functions are hoisted to the top of the script
+
+//the function of what you do is seperate from when you do it.
+
+
+
+
