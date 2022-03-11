d616e93c9926ec7dbe6dee9ade7d597fea5d320c
Michael Stokes <michael.stokes.347@gmail.com>
2022-03-11 12:35:03 -0600

    objects in jquery

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -9,3 +9,20 @@ $(document).ready(function() {
 $(document).ready(function() {
     alert( 'The DOM has finished loading!' );
 });
+
+var contents = $('#codeup').html();
+alert(contents);
+
+//help with walkthrough from Casey:
+
+//make html in js to practice using dom and jquery - per Casey
+
+$('body').append(getTemplates("George"));
+
+//HTML
+// /*HTML*
+
+// function getTemplates(name) {
+//     return <div>
+//         <h1>Templates ..... yeah!</h1>
+//         <p data-user-id=$"{studentObj.id}";
