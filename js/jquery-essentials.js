bde129f59ae75f6e69ae7e9395b917ba8790c57c
Michael Stokes <michael.stokes.347@gmail.com>
2022-03-14 11:39:08 -0500

    more recent js file

--- js/jquery-essentials.js
+++ js/jquery-essentials.js
@@ -1,14 +1,8 @@
-
-//TODO: refactor using jquery, try to refactor something else as well using jquery on another past work or project
 //VERSION USING VANILLA JS...
-(function(){
+(function () {
 
 // get the thing
     let navContent = document.getElementById("navContent");
-
-// log the thing to make sure you got the thing
-// console.log(navContent);
-
 
 // create an element
     let nameElement = document.createElement("p");
@@ -23,8 +17,6 @@
 // TODO: Add nameElement to the DOM - specifically in between the GitHub Logo and the ul on the navbar
     navContent.insertBefore(nameElement, navContent.children[1]);
 
-    // console.log($#navContent>"p")
-
     let contentDiv = document.querySelector('div.container-fluid.container-md.row.mx-md-auto.mt-4');
 
     let details = document.createElement('p');
@@ -32,9 +24,30 @@
 
     contentDiv.replaceChildren();
 
+    // Target a specific area on the DOM to insert our new paragraph
+    $('body > script').first().before(getNewParagraph())
+
+
+    //return a template string representing new html element(s)
+    //language=HTML
+    function getNewParagraph() {
+        return `
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A culpa, cum cupiditate eum facilis
+               inventore ipsum iure iusto maiores nam necessitatibus officiis quaerat quisquam recusandae suscipit
+               voluptas voluptatibus. Aliquid amet dicta, fugit inventore iure laborum nihil non nostrum obcaecati
+               officia officiis quasi, sequi sint sit sunt tenetur, voluptates! Adipisci asperiores at, consequatur
+               cumque dicta dignissimos earum ex exercitationem laudantium minima minus mollitia nam natus
+               necessitatibus nemo porro quis soluta voluptatem! A ad animi culpa, dolore eos est hic id itaque
+               laudantium libero mollitia natus nemo nihil nulla odit officiis perferendis praesentium quam quasi
+               repellat saepe similique ut voluptate? Commodi, modi.</p>
+          </div>
+        `
+    }
+
 
 // PRETEND THIS IS AN API CALL ACROSS THE INTERWEBS
-    function getGitHubInfo(){
+    function getGitHubInfo() {
         return {
             name: "vegetasrevenge",
             details: "I DO CODE REAL GUD 🧠"
