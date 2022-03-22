9502757999011a649c404a5046605e9c3a467484
Michael Stokes <michael.stokes.347@gmail.com>
2022-03-22 01:19:30 -0500

    kaizen

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,18 @@
 "use strict";
 
+//TODO: Make this the primary and take off inline js
+// mapboxgl.accessToken = 'pk.eyJ1IjoibWFya2FuZGVyc29uMCIsImEiOiJjbDEwbGQ5OW0wdjcwM2pzMWNkcHVvYnM5In0.yCiJw13Nfp6AnBLYSjiuyw';
+// const map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     style: 'mapbox://styles/markanderson0/cl11qnq87000c14s2oru88bx8', // style URL
+//     center: [-98.4916, 29.4252], // starting position [lng, lat]
+//     zoom: 8 // starting zoom
+// });
 
-
-mapboxgl.accessToken = YOUR_API_TOKEN_HERE;
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    zoom: 10,
-    center: [-98.4916, 29.4252]
-});
+// mapboxgl.accessToken = YOUR_API_TOKEN_HERE;
+// var map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/streets-v9',
+//     zoom: 10,
+//     center: [-98.4916, 29.4252]
+// });
