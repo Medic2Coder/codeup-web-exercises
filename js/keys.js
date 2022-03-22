ced64d912a80ac247ca0e95a3b4b57b6855e6e79
Michael Stokes <michael.stokes.347@gmail.com>
2022-03-21 23:02:09 -0500

    starting fresh

--- js/keys.js
+++ js/keys.js
@@ -1,3 +1,8 @@
 const OWM_KEY="5806ee52a7a4cf570e899e23ab573625";
 const NEWS_KEY="3a20be5784d14e198ef9b1fcad147bf4";
+const mapBoxToken = "sk.eyJ1IjoibWFya2FuZGVyc29uMCIsImEiOiJjbDExbDhjbGUwNTd5M2JtdzAxY2h1Zm4zIn0.4S0tkSR8he4wWd0wkyr-EA"
+
+// mapbox token
+// sk.eyJ1IjoibWFya2FuZGVyc29uMCIsImEiOiJjbDExbDhjbGUwNTd5M2JtdzAxY2h1Zm4zIn0.4S0tkSR8he4wWd0wkyr-EA
+// sk.eyJ1IjoibWFya2FuZGVyc29uMCIsImEiOiJjbDExbDhjbGUwNTd5M2JtdzAxY2h1Zm4zIn0.4S0tkSR8he4wWd0wkyr-EA
 
---
+++ js/weather.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+
+mapboxgl.accessToken = YOUR_API_TOKEN_HERE;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 10,
+    center: [-98.4916, 29.4252]
+});
