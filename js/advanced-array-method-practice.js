7c459dbbc6c68ec08edc4e54ea330b3d8b8c1289
Michael Stokes <michael.stokes.347@gmail.com>
2022-03-23 11:44:09 -0500

    notes

--- js/advanced-array-method-practice.js
+++ js/advanced-array-method-practice.js
@@ -2,3 +2,14 @@ const arr1 = [3,4,2,1];
     //arrays come with predfined methods that work on any array
 console.log(arr1.sort());
    //worked
+      //sort is an array method so it has to be attached to an array to work
+//method is a function that is never called by istself
+//method is always called using dot notation using some kind of data such as an array
+//must incluse paramter list with parenthesis, even if it's empty as in sort
+
+//it is possible to pass a function into another function's argument list as an argument
+ //concepts: higher order functions and callback functions
+
+//higher order functions: a function that takes another function as an argument, or returns a function as a return value
+
+//callback functions: a function that's passed into a higher order function as an argument, then used inside it (called back)
