7de01d13a65286819c5111e31cb002faf17d71fb
Michael Stokes <michael.stokes.347@gmail.com>
2022-03-23 10:04:28 -0500

    scoping in on scoping

--- js/es6-lec-notes.js
+++ js/es6-lec-notes.js
@@ -6,14 +6,39 @@ const super_secret_key = "password123";  // you can't change the value bc string
 
 //differences between let and var:
 
-{
-    {
-        {
-            {
-                let sample ="hi there"; //scoping is inside a curly brace
-            }
-            console.log(sample); //works if var  does not with let, let is only accessible within that scope
-        }
-    }
-}
+// {
+//
+//     {
+//         {
+//             {       let sample ="hi there"; //scoping is inside a curly brace
+//             }
+//             console.log(sample); //works if var  does not with let, let is only accessible within that scope// but will work inwardly
+//         }
+//     }
+// }
 
+
+// {                   let sample ="hi there"; //scoping is inside a curly brace
+//
+//     {
+//         {
+//             {
+//             }
+//             console.log(sample); //works if var  does not with let, let is only accessible within that scope// but will work inwardly
+//         }
+//     }
+// }
+//
+//
+
+//anonymous function is when you don't write specifically function instead just put let
+let sample = "hi there"; // if you define a variable inside a function probably wont be able to see it outside the function
+
+let scopeyBoi = function () {
+         console.log(sample);
+    }
+     scopeyBoi();
+    //but can see the variable inside the function if its placed outside on top
+
+
+//TODO: further read on lexical scoping
